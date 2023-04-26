1bb1cddfd0319cb6741eeb47d965481079f94e6d
soudomsinh <sengdaosinh@gmail.com>
2023-04-26 11:53:42 -0400

    Adding new items to array

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ let last_fruit2 = fruits_arrange[fruits_arrange.length-1]
 console.log(first_fruit);
 console.log(last_fruit_notArrange);
 console.log(last_fruit2);
+
+// adding new array items to array list with push
+
+console.log("Before = ", fruits);
+fruits.push("papaya", "grapes")
+console.log("After adding = " ,fruits);
