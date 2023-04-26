8553306fb4187caac8c67643535deb11b7c88b4e
soudomsinh <sengdaosinh@gmail.com>
2023-04-26 14:25:49 -0400

    Array-ForLoop

---
+++ index.js
@@ -0,0 +1,12 @@
+let fruits = ["Apple", "Orange", "Peach", "Pear", "Banana", "Pineapple","Mango","Strawberry"]
+
+fruits.push("pomelo")
+// i = 0 
+for(let i = 0; i<fruits.length; i++){
+    // console.log(fruits[i]);
+    // console.log("order =  ", i, " has value =", fruits[i]);
+    console.log("order =  ", (i+1), " has value =", fruits[i]);
+
+
+}
+
