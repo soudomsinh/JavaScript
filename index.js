23f45a78b15041c47c423994d11e31cc19fa04e1
soudomsinh <sengdaosinh@gmail.com>
2023-04-30 19:05:28 -0400

    JS DOM tutorial

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-// confirmation
+// JS DOM = JavaScript Document Object Method
 
-function deleteData(){
-    let result = confirm("Do you want to delete your data? ")
-    if (result) {
-        console.log("Deleted successfully");
-    }else{
-        console.log("Delete cancelled");
-    }
-}
+let p = document.getElementsByTagName('p');
+console.log(p);
+
+// let mydemo = document.getElementById('demo');
+let mydemo2 = document.querySelector('#demo');
+// console.log(mydemo);
+console.log(mydemo2);
+
+
+let mycss = document.querySelector('.mycss');
+console.log(mycss);
+
+
+function displayText1() {
+    mydemo.innerText= "<strong>Learning JavaScript and DOM basic </strong>";
+
+
+    
+};
+
+function displayText2() {
+    mydemo.innerHTML= "<h1>Learning JavaScript and DOM basic </h1>";
+
+};
+
+
+let j = 10;
+let i = 20;
+function displayText3() {
+    mydemo.innerText= "Rendering variable j = " + j + "," + "Rendering variable i = " + i ;
+    mydemo.innerText= `Rendering j = ${i} and j = ${j}` ;
+
+
+
+    
+};
+
+const a = document.querySelector("#demo");
+const b = document.querySelector('.mycss');
+const c = document.querySelectorAll('p')
+
+console.log(a);
+console.log(b);
+console.log(c);
+
