6cd694934123cd262fbda695279727593befeb4e
soudomsinh <sengdaosinh@gmail.com>
2023-04-30 19:46:04 -0400

    JS DOM Practice

--- index.js
+++ index.js
@@ -1,47 +1,27 @@
-// JS DOM = JavaScript Document Object Method
-
-let p = document.getElementsByTagName('p');
-console.log(p);
-
-// let mydemo = document.getElementById('demo');
-let mydemo2 = document.querySelector('#demo');
-// console.log(mydemo);
-console.log(mydemo2);
+const title_Element = document.getElementById('title'); // refer by id attribute
+const content_Element = document.querySelector('.content'); // refer by class attribute
+const All_Element = document.querySelectorAll('p');
+const box = document.querySelector('.box');
 
 
-let mycss = document.querySelector('.mycss');
-console.log(mycss);
+
+function displayText() {
+    title_Element.style.color = "red";
+    title_Element.style.backgroundColor="yellow"
+    title_Element.style.fontSize = "60px"
+    content_Element.setAttribute('class', 'newContent' )
+
+}
 
 
-function displayText1() {
-    mydemo.innerText= "<strong>Learning JavaScript and DOM basic </strong>";
-
-
+function orangeMode() {
+    box.setAttribute('class', 'orange' )
     
-};
-
-function displayText2() {
-    mydemo.innerHTML= "<h1>Learning JavaScript and DOM basic </h1>";
-
-};
+}
 
 
-let j = 10;
-let i = 20;
-function displayText3() {
-    mydemo.innerText= "Rendering variable j = " + j + "," + "Rendering variable i = " + i ;
-    mydemo.innerText= `Rendering j = ${i} and j = ${j}` ;
-
-
-
+function purpleMode() {
+    box.setAttribute('class', 'purple' )
     
-};
-
-const a = document.querySelector("#demo");
-const b = document.querySelector('.mycss');
-const c = document.querySelectorAll('p')
-
-console.log(a);
-console.log(b);
-console.log(c);
+}
 
