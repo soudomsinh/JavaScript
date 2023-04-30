04942fc328d8dd2252f3e8a5f075966c7992f0d2
soudomsinh <sengdaosinh@gmail.com>
2023-04-30 17:48:12 -0400

    JS confirmation

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-// JavaScript Object
+// confirmation
 
-// let Name_of_Object = {propertyName:value}
-
-// For example
-
-let product = {
-    names: "mouse", 
-    price: 20,
-    color: "black",
-    category: "electronic",
-    size: "regular",
-};
-
-// // method 1
-console.log("name of product is =", product.names);
-console.log("price of product is =", product.price);
-
-// // method 2
-console.log("color of product is =", product["color"]);
-
-// Object with Method
-console.log(" /////below is how to use Object with Method/////");
-let product2 = {
-    names: "mouse", 
-    price: 25,
-    color: "black",
-    category: "electronic",
-    size: "regular",
-    displyProduct:function(){
-        return "product name = " + this.names + "price = " + this.price + "category = " + this.category
-    },
-    discount:function(){
-        return this.price - 10;
-    },
-    getColor:function(){
-        return this.color;
+function deleteData(){
+    let result = confirm("Do you want to delete your data? ")
+    if (result) {
+        console.log("Deleted successfully");
+    }else{
+        console.log("Delete cancelled");
     }
-};
-console.log(product2.displyProduct());
-console.log(product2.discount());
-console.log(product2.getColor());
-
-
-
-// display on the website 
-// document.write(product.displyProduct());
+}
