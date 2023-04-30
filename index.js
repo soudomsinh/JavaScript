f729fee4120cd28784e1f239afb58a7934f79fcd
soudomsinh <sengdaosinh@gmail.com>
2023-04-30 15:47:47 -0400

    Update Array-string

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 let color = ["blue", "green", "yellow"]
 
-let g = color.toString();
-let h = color.join("|");
-console.log(g);
-console.log(h);
-console.log(typeof(g));
+// let g = color.toString();
+// let h = color.join("|");
+// console.log(g);
+// console.log(h);
+// console.log(typeof(g));
+
+let pop_pop = color.pop()
+console.log(pop_pop);
+console.log(color);
