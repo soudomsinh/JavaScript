bab65b1b98b4dce60df9e41437f309266b31688d
soudomsinh <sengdaosinh@gmail.com>
2023-05-19 06:46:10 -0400

    Spread operator

---
+++ JS-ES6/spreadOperator.js
@@ -0,0 +1,15 @@
+let country = ["US", "India", "China"]
+let newCountry = ["UK", "Thailand", "Singapore", ...country]
+// console.log(newCountry);
+// let newCountry2 = ["UK", "Thailand", "Singapore", ...country]
+// console.log(newCountry2);
+
+// console.log("############");
+newCountry.push(...country)
+console.log(newCountry);
+
+
+`
+Spread operator is the ... infront of country
+
+`
